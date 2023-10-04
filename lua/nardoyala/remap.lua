0bda469181a795262df736756984873678a151be
Bernardo Ayala <ba@bernardoayala.com>
2023-10-03 21:43:32 -0400

    add remap to copy to system clipboard

--- lua/nardoyala/remap.lua
+++ lua/nardoyala/remap.lua
@@ -1,5 +1,7 @@
+-- Change leader key
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selected lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -8,4 +10,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Replace all occurrences of the word under the cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Copy to system clipboard
+vim.keymap.set("v", "<C-c>", '"+y')
