3ee1c8c19e0a5bdbd8822b1daaaf97f7f67b8d70
Bernardo Ayala <ba@bernardoayala.com>
2023-10-17 19:43:32 -0400

    add more remaps

--- lua/nardoyala/remap.lua
+++ lua/nardoyala/remap.lua
@@ -10,11 +10,26 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Keep cursor in place when using J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Paste replacing the current selection without yanking it
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
 -- Replace all occurrences of the word under the cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Copy to system clipboard
-vim.keymap.set("v", "<C-c>", '"+y')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+-- Delete to void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- When on vertical mode, make "control + c" equivalent to escape
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Switch between tabs
 vim.keymap.set("n", "]t", "gt")
@@ -24,3 +39,4 @@ vim.keymap.set("n", "<leader>2", "2gt")
 vim.keymap.set("n", "<leader>3", "3gt")
 vim.keymap.set("n", "<leader>4", "4gt")
 vim.keymap.set("n", "<leader>5", "5gt")
+
