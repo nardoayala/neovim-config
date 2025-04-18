4cc16a7c1f19eb34e6a7b1a90c3e9c81fefe5541
Bernardo Ayala <ba@bernardoayala.com>
2025-04-18 10:37:58 -0400

    tweaks for lsp and fugitive

--- lua/nardo/lazy/fugitive.lua
+++ lua/nardo/lazy/fugitive.lua
@@ -1,15 +1,5 @@
 return {
   'tpope/vim-fugitive',
-  cmd = {
-    "Git",
-    "Gstatus",
-    "Gblame",
-    "Gpush",
-    "Gpull",
-  },
-  keys = {
-    { "<leader>gs", "<cmd>Git<cr>", desc = "Fugitive status" },
-  },
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
   end
